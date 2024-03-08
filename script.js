b6d81507be3643372977f8c3bcc55e100bb20bf1
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-03-08 11:35:09 +0100

    add style to active link

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
 
+// Toggle the active class on the nav-links
 burger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+// Add class active link if the link is clicked
+const links = document.querySelectorAll(".nav-links li a");
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    links.forEach((link) => link.classList.remove("active"));
+    link.classList.add("active");
+    // Remove the active class from the nav-links
+    navLinks.classList.remove("active");
+  });
 });
