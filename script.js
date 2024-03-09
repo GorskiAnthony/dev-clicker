51b413eb64f90333d8e84b956147c8242a4226d8
Anthony Gorski <anthony.developpeurweb@gmail.com>
2024-03-09 21:53:01 +0100

    add template cardShop

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
 const burger = document.querySelector(".burger");
 const navLinks = document.querySelector(".nav-links");
+let shopItems = document.querySelector(".shop-items");
+
+const shops = [
+  {
+    title: "Automatisation",
+    price: "10",
+    description: "Automatisation is the key to success",
+    img: "automatisation",
+  },
+  {
+    title: "Coffee",
+    price: "10",
+    description: "Coffee is the key to success",
+    img: "coffee",
+  },
+  {
+    title: "Coffee premium",
+    price: "10",
+    description: "Coffee is the key to success (but premium)",
+    img: "coffee_plus",
+  },
+  {
+    title: "Congrats",
+    price: "10",
+    description: "Congrats is the key to success",
+    img: "congrat",
+  },
+  {
+    title: "Time",
+    price: "10",
+    description: "Time is the key to success",
+    img: "time",
+  },
+];
 
 // Toggle the active class on the nav-links
 burger.addEventListener("click", () => {
@@ -16,3 +50,13 @@ links.forEach((link) => {
     navLinks.classList.remove("active");
   });
 });
+
+// Create the shop items
+
+let html = shops
+  .map((shop) => {
+    return createCard(shop.title, shop.price, shop.description, shop.img);
+  })
+  .join("");
+
+shopItems.innerHTML = html;
